3ffbdd68494d035ec69bb7924e934700b8b813d6
ankit <devaniankit1990@gmail.com>
2021-06-17 12:54:03 +0530

    Final Commit

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ var requests = require("requests");
 
 const homeFile = fs.readFileSync("home.html","utf-8");
 const replaceVal =(tempVal, orgVal) =>{
-  let temperature = tempVal.replace("{tempval}",orgVal.main.temp);
-  console.log(temperature);
+  let temperature = tempVal.replace("{% tempval %}",orgVal.main.temp);
+  temperature = temperature.replace("{% tempmax %}",orgVal.main.temp_min);
+  temperature = temperature.replace("{% tempmin %}",orgVal.main.temp_max);
+  temperature = temperature.replace("{% location %}",orgVal.name);
+  temperature = temperature.replace("{% country %}",orgVal.sys.country);
+  temperature = temperature.replace("{% tempstatus %}",orgVal.weather[0].main);
+  return temperature   
  }
 const server = http.createServer((req,res)=>{
 if(req.url == "/"){
@@ -16,13 +21,13 @@ if(req.url == "/"){
       const arrData = [objData];
       console.log(arrData);
       console.log(arrData[0].main.temp);
-      const realTimeData = arrData.map((val)=>{
-        replaceVal(homeFile, val);
-      });
+      const realTimeData = arrData.map(val =>replaceVal(homeFile, val)).join("");
+      res.write(realTimeData);
+      //console.log(realTimeData);
     })
     .on('end', function (err) {
       if (err) return console.log('connection closed due to errors', err);
-      console.log('end');
+      res.end();
     });
 }
 });
